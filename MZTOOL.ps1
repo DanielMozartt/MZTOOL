b5063a43906144e4124438e9aac8cffda9338c02
DanielMozartt <115650498+DanielMozartt@users.noreply.github.com>
2024-06-24 22:26:23 -0300

    Update MZTOOL.ps1

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -6,7 +6,7 @@ Expand-Archive -LiteralPath 'C:\TOOL\MZT_TOOL.zip' -DestinationPath 'C:\TOOL'
 
 del 'C:\TOOL\MZT_TOOL.zip'
 
-C:\TOOL\MZT_TOOL.exe
+Start-Process -FilePath "C:\TOOL\MZT_TOOL.exe" -Verb RunAs
 
 
 
