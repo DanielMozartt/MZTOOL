2edcb0fc205b6b1804f0ecc40df0dc7b2d617be2
Mozart Informatica <115650498+DanielMozartt@users.noreply.github.com>
2024-06-27 16:45:56 -0300

    UP

--- WINUPDATE.ps1
+++ WINUPDATE.ps1
@@ -9,20 +9,5 @@ Get-WindowsUpdate -Download -AcceptAll -Install -ForceInstall -IgnoreReboot -Ver
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -Install -ForceInstall -IgnoreReboot -Verbose
 }
 
-<<<<<<< HEAD
-=======
-#Start-Process powershell -Verb runAs {Install-Module PSWindowsUpdate -AllowClobber -Force}
-
-#Start-Process powershell -Verb runAs {Import-Module PSWindowsUpdate -Force} 
-
-Install-WindowsUpdate -MicrosoftUpdate -WindowsUpdate -AcceptAll -Install -ForceDownload -ForceInstall  -IgnoreReboot
-
-#Install-PackageProvider -Name NuGet -Force
-
-#Install-Module PSWindowsUpdate -AllowClobber -Force
-
-#Import-Module PSWindowsUpdate -Force
-
->>>>>>> fe3301a6362284430666c5b0ccbf606400be16bc
 PAUSE
 
