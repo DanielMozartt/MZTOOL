7c25ecab8247d7fdb6715739c721c92ca9194a17
Mozart Informatica <115650498+DanielMozartt@users.noreply.github.com>
2024-06-29 20:13:37 -0300

    UP

--- BETA.ps1
+++ BETA.ps1
@@ -2,7 +2,9 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
 Start-Process powershell -Verb runAs { irm $RUN | iex }
+
 $RUN = "
+
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
 $TOOL = "C:\TOOL"
