0e8e517577c61728dde6c8c964f8e88d21dac8fd
Mozart Informatica <115650498+DanielMozartt@users.noreply.github.com>
2024-06-29 20:13:14 -0300

    UP

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -1,5 +1,11 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
 
+Start-Process powershell -Verb runAs { irm $RUN | iex }
+
+$RUN = "
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
 $TOOL = "C:\TOOL"
 
 [System.IO.Directory]::CreateDirectory($TOOL)
@@ -112,3 +118,4 @@ Remove-Item -Path $TOOL\#TOOL#ZIP\DRIVER_BOOSTER_7.5_PORTABLE -Recurse -Force -E
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 1 /f
 
 exit
+"
