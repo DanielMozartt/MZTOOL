40c2e3a6090e5487852e35e6ac02fb782916a41c
Mozart Informatica <115650498+DanielMozartt@users.noreply.github.com>
2024-07-01 22:55:43 -0300

    UP

--- BETA.ps1
+++ BETA.ps1
@@ -1,5 +1,11 @@
-
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+#Sincroniza o Horário do Sistema com o servidor Time.Windows.
+
+net start w32time
+w32tm /resync /force
+
+#Criação do diretório C:\TOOL.
 
 $TOOL = "C:\TOOL"
 
@@ -7,32 +13,50 @@ $TOOL = "C:\TOOL"
 $TOOLFOLDER = Get-Item $TOOL 
 $TOOLFOLDER.Attributes = "Hidden" 
 
+#Instalação do software AnyDesk Portátil na área de trabalho do usuário.
+
 Start-Process powershell -Verb runAs -WindowStyle hidden {iwr -Uri "https://download.anydesk.com/AnyDesk-CM.exe" -OutFile "$home\Desktop\AnyDesk.exe"} | Out-Null
 
 Start-Process powershell -Verb runAs {
+
+#Instalação do Winget.
 
 Install-PackageProvider -Name NuGet -Force | Out-Null
 Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
 Repair-WinGetPackageManager
 
-winget add "Adobe.Acrobat.Reader.64-bit" "Microsoft.Powershell" "Google.Chrome"  --accept-source-agreements --accept-package-agreements
+#Instalação dos softwares Acrobat Reader, Microsoft Powershell 7+, Google Chrome. 
 
 winget add "Adobe.Acrobat.Reader.64-bit" "Microsoft.Powershell" "Google.Chrome"  --accept-source-agreements --accept-package-agreements
 
 winget add "Adobe.Acrobat.Reader.64-bit" "Microsoft.Powershell" "Google.Chrome"  --accept-source-agreements --accept-package-agreements
+
+winget add "Adobe.Acrobat.Reader.64-bit" "Microsoft.Powershell" "Google.Chrome"  --accept-source-agreements --accept-package-agreements
+
+exit
 
 }
 
 Start-Process powershell -Verb runAs {
-
+    
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+#Instalação do módulo Windows Update.
+
 Install-PackageProvider -Name NuGet -Force
 Install-Module PSWindowsUpdate -AllowClobber -Force
 Import-Module PSWindowsUpdate -Force 
+
+#Instalação de novas atualizações do Windows através do Windows update.
+
 Get-WindowsUpdate -Download -AcceptAll -Install -ForceInstall -IgnoreReboot -Verbose
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -Install -ForceInstall -IgnoreReboot -Verbose
 
+exit
+
 }
+
+# Download do arquivo #TOOL#ZIP.zip.
 
 $webClient = New-Object -TypeName System.Net.WebClient
 $task = $webClient.DownloadFileTaskAsync("https://seulink.net/TOOLZIP", "$TOOL\#TOOL#ZIP.zip")
@@ -72,27 +96,43 @@ function convertFileSize {
 Unregister-Event -SourceIdentifier WebClient.DownloadProgressChanged
 $webClient.Dispose()
 
+#Extração do arquivo #TOOL#ZIP.zip para a pasta  $TOOL\#TOOL#ZIP.
+
 Expand-Archive -LiteralPath $TOOL\#TOOL#ZIP.zip -DestinationPath $TOOL
+
+#Instalação do software AnyDesk Portátil na pasta $TOOL\#TOOL#ZIP.
 
 Start-Process powershell -Verb runAs -WindowStyle hidden {iwr -Uri "https://download.anydesk.com/AnyDesk-CM.exe" -OutFile "$TOOL\#TOOL#ZIP\AnyDesk.exe"}
 
+#Desativar o UAC.
+
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+
+#Deletar o arquivo #TOOL#ZIP.zip.
 
 del $TOOL\#TOOL#ZIP.zip
 
-Expand-Archive -LiteralPath $TOOL\#TOOL#ZIP\DRIVER_BOOSTER_7.5_PORTABLE.zip -DestinationPath $TOOL\#TOOL#ZIP\
+#Extração e inicialização do software Driver Booster.
+
+Expand-Archive -LiteralPath $TOOL\#TOOL#ZIP\DRIVER_BOOSTER.zip -DestinationPath $TOOL\#TOOL#ZIP\
 
 start $TOOL\#TOOL#ZIP\DRIVER_BOOSTER_7.5_PORTABLE\DriverBoosterPortable.exe
 
-Invoke-Command -ScriptBlock {Start-Process "$TOOL\OFFICE\2007\SaveAsPdf.EXE" -ArgumentList "/quiet" -Wait}
+#Intalação do software Microsoft Office 2007 e ADD-in's SaveAsPDF e ODFAddIn.
 
-msiexec /i $TOOL\OFFICE\2007\ODF\OdfAddInForOfficeSetup.msi /q ALLUSERS=1
+Invoke-Command -ScriptBlock {Start-Process "$TOOL\OFFICE\2007\Setup.exe" -ArgumentList "/adminfile Silent.msp" -Wait}
 
-Invoke-Command -ScriptBlock {Start-Process "$TOOL\OFFICE\2007\SETUP\setup.exe" -ArgumentList "/adminfile Silent.msp" -Wait}
+#Verificação e instalação de atualizações de softwares instalados e do Windows via Winget e Windows Update.
 
 winget upgrade --all --accept-source-agreements --accept-package-agreements --silent --purge --skip-dependencies --include-unknown
 
+Get-WindowsUpdate -Download -AcceptAll -Install -ForceInstall -IgnoreReboot -Verbose
+
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -Install -ForceInstall -IgnoreReboot -Verbose
+
 Start-Sleep -Seconds 20
+
+#Remover arquivos temporários.
 
 Remove-Item -Path $env:TEMP\* -Recurse -Force -ErrorAction SilentlyContinue 
 
@@ -100,15 +140,21 @@ Remove-Item -Path $env:C:\Windows\temp\* -Recurse -Force -ErrorAction SilentlyCo
 
 Remove-Item -Path $env:C:\Windows\Prefetch\* -Recurse -Force -ErrorAction SilentlyContinue
 
-taskkill /f /IM DriverBooster.exe /T
-
 Start-Sleep -Seconds 500
 
-Remove-Item -Path $TOOL\#TOOL#ZIP\DRIVER_BOOSTER_7.5_PORTABLE -Recurse -Force -ErrorAction SilentlyContinue
+#Finaliza o serviço do software Driver Booster e deleta a pasta temporária do mesmo.
 
-#Remove-Item -Path $TOOL -Recurse -Force -ErrorAction SilentlyContinue
+taskkill /f /IM DriverBooster.exe /T
+
+Start-Sleep -Seconds 20
+
+Remove-Item -Path $TOOL\#TOOL#ZIP\DRIVER_BOOSTER -Recurse -Force -ErrorAction SilentlyContinue
+
+#Adiciona variável de ambiente %TOOL%.
 
 [Environment]::SetEnvironmentVariable("TOOL", "C:\TOOL", "Machine")
+
+#Reativar UAC.
 
 REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 1 /f
 
