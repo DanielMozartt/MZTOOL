8512a121d18314b2cee0a3b63f38d2407f818590
Mozart Informatica <115650498+DanielMozartt@users.noreply.github.com>
2024-07-02 18:32:56 -0300

    UP

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -38,9 +38,9 @@ exit
 }
 
 Start-Process powershell -Verb runAs -WindowStyle hidden {
-    
-#Instalação do módulo Windows Update.    
 
+#Instalação do módulo Windows Update.   
+ 
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 Install-PackageProvider -Name NuGet -Force
 Install-Module PSWindowsUpdate -AllowClobber -Force
