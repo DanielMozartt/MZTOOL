d3adbe192f2bc6136eca515ff7a2a5c5b9615ecc
Mozart Informatica <115650498+DanielMozartt@users.noreply.github.com>
2024-07-02 22:54:03 -0300

    UP

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -4,45 +4,50 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 function DisplayMenu {
     Clear-Host
     Write-Host "
-    +===============================================+
-    |  MZTOOL - MOZART INFORMÁTICA - DANIEL MOZART  | 
-    +===============================================+
-    |                                               |
-    |1| INSTALAR SOFTWARES E ATUALIZAÇÕES DO SISTEMA|
-    |2| DIAGNÓSTICO DE HARDWARE E SISTEMA           |
-    |3| SAIR                                        |
-    +===============================================+
-    
+    ______________________________________________________
+    |                                                    |
+    |                      MZTOOL                        |
+    | _________________________________________________  | 
+    |                                                    |
+    |                                                    |
+    | |1| INSTALAR SOFTWARES E ATUALIZAÇÕES DO SISTEMA   |
+    | |2| DIAGNÓSTICO DE HARDWARE E SISTEMA              |
+    | |3| SAIR                                           |
+    |                                                    |
+    |                                                    |
+    |                 MOZART INFORMÁTICA | DANIEL MOZART |
+    |____________________________________________________|
     "
     
-    $MENU = Read-Host "OPTION"
+    $MENU = Read-Host "INSIRA O NÚMERO CORRESPONDENTE A OPÇÃO DESEJADA"
     Switch ($MENU)
     {
     1 {
-    #OPÇÃO 1 - INSTALAR SOFTWARES E ATUALIZAÇÕES DO SISTEMA
-    $OPTION1 = Read-Host "HOST"
-    Test-Connection -ComputerName $OPTION1
-    Start-Sleep -Seconds 2
+    #OPÇÃO 1 - INSTALAR SOFTWARES E ATUALIZAÇÕES DO SISTEMA.
+    $OPTION1 = Start-Process "Powershell" -Verb runAs {irm https://raw.githubusercontent.com/DanielMozartt/MZTOOL/main/INSTALL.ps1 | iex}
     DisplayMenu
     }
     2 {
-    #OPTION2 - DISPLAY MESSAGE
-    $OPTION2 = Read-Host "MESSAGE"
-    Write-Host "MESSAGE: $OPTION2"
-    Start-Sleep -Seconds 2
+    #OPÇÃO 1 - DIAGNÓSTICO DE HARDWARE E SISTEMA.
+    $OPTION2 = Write-Host "EM DESENVOLVIMENTO - SELECIONE UMA NOVA OPÇÃO"
+    Start-Sleep -Seconds 5
     DisplayMenu
     }
     3 {
-    #OPTION3 - EXIT
-    Write-Host "Bye"
+    #OPÇÃO 3 - ENCERRAR SISTEMA.
+    $OPTION3
+    Write-Host "ENCERRANDO MZTOOL"
+    Start-Sleep -Seconds 5
     Break
     }
     default {
-    #DEFAULT OPTION
-    Write-Host "Option not available"
+    #ENTRADA INVÁLIDA.
+    Write-Host "OPÇÃO INVÁLIDA. INSIRA O NÚMERO CORRESPONDENTE A OPÇÃO DESEJADA"
     Start-Sleep -Seconds 2
-    DisplayMenu
+    Exit
     }
     }
 }
     DisplayMenu
+
+    Exit
