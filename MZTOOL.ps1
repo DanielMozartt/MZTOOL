937c0c6c82e73fdd08bdefec13b40c2286bf0bb6
Mozart Informatica <115650498+DanielMozartt@users.noreply.github.com>
2024-07-02 23:32:15 -0300

    UP

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -1,5 +1,7 @@
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 
+$Host.UI.RawUI.BackgroundColor = "DarkBlue"
+
 #MENU MZTOOL.
 function DisplayMenu {
     Clear-Host
@@ -22,36 +24,42 @@ function DisplayMenu {
     $MENU = Read-Host "INSIRA O NÚMERO CORRESPONDENTE A OPÇÃO DESEJADA"
     Switch ($MENU)
     {
+
     1 {
     #OPÇÃO 1 - INSTALAR SOFTWARES E ATUALIZAÇÕES DO SISTEMA.
-    $OPTION1 = 
+
     Write-Host "EM INSTALAÇÃO"
-    Start-Process "Powershell" -Verb runAs -WindowStyle Hidden -Wait{irm https://raw.githubusercontent.com/DanielMozartt/MZTOOL/main/INSTALL.ps1 | iex}
+    Start-Process "Powershell" -Verb runAs -WindowStyle Hidden -Wait{Invoke-RestMethod https://raw.githubusercontent.com/DanielMozartt/MZTOOL/main/INSTALL.ps1 | Invoke-Expression}
     Write-Host "ENCERRANDO MZTOOL"
     Start-Sleep -Seconds 5
     Exit
     }
+
     2 {
-    #OPÇÃO 1 - DIAGNÓSTICO DE HARDWARE E SISTEMA.
-    $OPTION2 = Write-Host "EM DESENVOLVIMENTO - SELECIONE UMA NOVA OPÇÃO"
+    #OPÇÃO 2 - DIAGNÓSTICO DE HARDWARE E SISTEMA.
+
+    Write-Host "EM DESENVOLVIMENTO - SELECIONE UMA NOVA OPÇÃO"
     Start-Sleep -Seconds 5
-    Exit
+    DisplayMenu
     }
+
     3 {
     #OPÇÃO 3 - ENCERRAR SISTEMA.
-    $OPTION3
+
     Write-Host "ENCERRANDO MZTOOL"
     Start-Sleep -Seconds 5
     Break
     }
+
     default {
     #ENTRADA INVÁLIDA.
+
     Write-Host "OPÇÃO INVÁLIDA. INSIRA O NÚMERO CORRESPONDENTE A OPÇÃO DESEJADA"
-    Start-Sleep -Seconds 2
-    Exit
+    Start-Sleep -Seconds 3
+    DisplayMenu
     }
     }
 }
     DisplayMenu
 
-    Exit
+Exit
