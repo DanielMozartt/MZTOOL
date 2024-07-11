ec930b37d031917b48ad78f926ba32b21614ea29
Mozart Informatica <115650498+DanielMozartt@users.noreply.github.com>
2024-07-11 10:07:44 -0300

    up

--- BETA.ps1
+++ BETA.ps1
@@ -37,23 +37,11 @@ else {
 
 #MENU MZTOOL -----------------------------------------------------
 function Set-ConsoleWindow {
-    param(
-        [int]$Width,
-        [int]$Height
-    )
-
-    $WindowSize = $Host.UI.RawUI.WindowSize
-    $WindowSize.Width = [Math]::Min($Width, $Host.UI.RawUI.BufferSize.Width)
-    $WindowSize.Height = $Height
-
-    try {
-        $Host.UI.RawUI.WindowSize = $WindowSize
-    }
-    catch [System.Management.Automation.SetValueInvocationException] {
-        $Maxvalue = ($_.Exception.Message | Select-String '\d+').Matches[0].Value
-        $WindowSize.Height = $Maxvalue
-        $Host.UI.RawUI.WindowSize = $WindowSize
-    }
+    $H = Get-Host
+    $Win = $H.UI.RawUI.WindowSize
+    $Win.Height = 10
+    $Win.Width = 10
+    $H.UI.RawUI.Set_WindowSize($Win)
 }
 function DisplayMenu {
     Clear-Host
