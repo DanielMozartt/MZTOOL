dfdc1fa42cb115d7d51deae54627429414c5c706
Mozart Informatica <115650498+DanielMozartt@users.noreply.github.com>
2024-07-15 12:57:54 -0300

    up

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -758,11 +758,12 @@ function Office365 {
     $TOOLFOLDER.Attributes = 'Hidden'  
     
     $XML.save("$TOOL\OFFICE\365\OFFICE365.xml") 
-
+    $TOOL = 'C:\TOOL'
     $365XML = "$TOOL\OFFICE\365\OFFICE365.xml"
 
     Winget Install --Id Microsoft.Office --Override "/configure $365XML" --Accept-Source-Agreements --Accept-Package-Agreements
     
+    Clear-Host
 }
     
 function Office2007 {
