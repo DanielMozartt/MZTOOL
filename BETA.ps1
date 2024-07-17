e2b81e5fe84cd16705d9481a70d1c8a958498d52
Mozart Informatica <115650498+DanielMozartt@users.noreply.github.com>
2024-07-16 23:37:58 -0300

    up

--- BETA.ps1
+++ BETA.ps1
@@ -15,15 +15,14 @@ if ($myWindowsPrincipal.IsInRole($adminRole)) {
 
     $Host.UI.RawUI.WindowTitle = 'MZTOOL ⭡'
     $Host.UI.RawUI.BackgroundColor = 'DarkBlue'
-    $Host.UI.RawUI.ForegroundColor = 'Black'
-    $host.UI.RawUI.WindowPosition = New-Object System.Management.Automation.Host.Coordinates(0, 160)
+    $host.UI.RawUI.WindowPosition = New-Object System.Management.Automation.Host.Coordinates(0, 500)
     $host.UI.RawUI.BufferSize
-    $host.UI.RawUI.BufferSize = New-Object System.Management.Automation.Host.Size(120, 999)
+    <#$host.UI.RawUI.BufferSize = New-Object System.Management.Automation.Host.Size(120, 999)
     $H = Get-Host
     $Win = $H.UI.RawUI.WindowSize
     $Win.Height = 20
     $Win.Width = 58
-    $H.UI.RawUI.Set_WindowSize($Win)
+    $H.UI.RawUI.Set_WindowSize($Win)#>
     
     Clear-Host
 }
