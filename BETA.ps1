2e46d34e4df2c6ced4b366962f2294dc7b276b63
Mozart Informatica <115650498+DanielMozartt@users.noreply.github.com>
2024-07-18 19:49:17 -0300

    up

--- BETA.ps1
+++ BETA.ps1
@@ -259,6 +259,7 @@ ______________________________________________________
                         EnvTool
                         
                         ToolDir 
+                        
                         DownloadMztool 
                                      
                         Diagnostics32 
