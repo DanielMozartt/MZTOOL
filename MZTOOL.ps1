13da69c5f2159571a039052830505b3663980a4c
Mozart Informatica <115650498+DanielMozartt@users.noreply.github.com>
2024-07-29 15:10:46 -0300

    up

--- MZTOOL.ps1
+++ MZTOOL.ps1
@@ -227,7 +227,11 @@ ______________________________________________________
                                 
                         ToolDir 
 
-                        DownloadMztool      
+                        Start-Sleep -Seconds 1
+
+                        DownloadMztool 
+                        
+                        Start-Sleep -Seconds 1     
                         
                         Diagnostics64
                                                 
